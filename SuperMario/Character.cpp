12dfdd9c95dd67c2c46c9bab2c1e81c0a8331440
NguyenHoang <nguyenislearning03@gmail.com>
2024-12-01 15:55:22 +0700

    Character Update: Mario
    Constructor for Mario, Initialize Mario body and fixtures in a Box2D world, Initalize Logic for update Mario each elasped timestamp

--- SuperMario/Character.cpp
+++ SuperMario/Character.cpp
@@ -10,18 +10,17 @@ Character* CharacterFactory :: createCharacter(CharacterType type, float x, floa
     }
 }
 
+
 Mario::Mario(float x, float y) {
-    acc[0] = 57;
-    acc[1] = 80;
-    spd[0] = 0;
-    spd[1] = 70;
-    startJumpPosition = 500;
-    changeStateCounter = 0;
-    goRight = goUp = goLeft = goDown = jumping = onGround = false;
-    
+    position.x = x;
+    position.y = y;
+    goRight = goUp = goLeft = goDown = false;
+    movementVelocity = 3.0f;
+    jumpVelocity = 1.0f;
+    angle = 0.0f;
 
     // Set Mario Sprite Properties
-    if (!texture.loadFromFile("D:/CS202-SuperMarioGame/SuperMario/Resource/Mario/mario.png")) { cout << "Can't load MARIO_CHARACTER\n"; }
+    if (!texture.loadFromFile("./Resource/Mario/mario.png")) { cout << "Can't load MARIO_CHARACTER\n"; }
     //if (!marioSuperTexture.loadFromFile(MARIO_SUPER_CHARACTER)) { std::cout << "Can't load MARIO_SUPER_CHARACTER\n"; }
     texture.setSmooth(true);
     sprite.setTexture(texture);
@@ -30,15 +29,50 @@ Mario::Mario(float x, float y) {
 
     //Define here
     //Sound buffer and sound effect : Jump, d
-};
+}
 
 
 void Mario :: setPosition(float x, float y) {
     sprite.setPosition(x, y);
 }
 
-void Mario :: draw(RenderWindow& window) {
+void Mario :: draw(RenderWindow& window, int state) {
     window.draw(sprite);
+}
+ 
+void Mario::Begin() {
+    //Initialize a body of Character in the b2World.
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody; // specify type of body
+    bodyDef.position.Set(position.x, position.y); //Set position
+    dynamicBody = Physics::world.CreateBody(&bodyDef);
+
+    //Create and add Fixtures for the body => This will set for collision
+    b2FixtureDef fixtureDef;
+    b2PolygonShape shape{};
+    shape.SetAsBox(0.5f, 1.0f);
+
+    fixtureDef.shape = &shape;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+
+    //Create fixture for body => Done;
+    dynamicBody->CreateFixture(&fixtureDef);
+}
+
+void Mario::Update(float deltaTime) {
+    float move = movementVelocity;
+    if (Keyboard::isKeyPressed(Keyboard::LShift))
+        move *= 2;
+    
+    //Update position and angle
+    position = Vector2f(dynamicBody->GetPosition().x, dynamicBody->GetPosition().y);
+    angle = dynamicBody->GetAngle() * (180.0f / PI); //Angle calculated in radian
+
+}
+
+void Mario::draw(RenderWindow& window, int state) {
+    return;
 }
 
 
--- SuperMario/Character.h
+++ SuperMario/Character.h
@@ -1,24 +1,32 @@
 #pragma once
 #include "INCLUDE.h"
+#include "Physics.h"
 enum CharacterType {MARIO, LUIGI, CustomCharacter};
 
 class Character {
-    const float movementSpeed = 3.0f;
+public:
+    float movementVelocity;
+    float jumpVelocity;
+    Vector2f position;
+    float angle;
+
+
+    virtual ~Character() = default;
+    virtual void Begin() = 0;
+    virtual void Update(float deltaTime) = 0;
+    virtual void draw(RenderWindow& window, int state) = 0;
 protected:
     Texture superTexture;
     Texture texture;
     Sprite sprite;
     string name;
-    Vector2f position{};
+    b2Body* dynamicBody = nullptr;
+    int changeStateCounter; //3 states (Small, Big, Super) => we will need 3 bodies for each state.
+    //bool jumping, onGround;
 
-    int changeStateCounter; //3 states (Small, Big, Super)
-    bool jumping, onGround;
+    //float spd[2]; //Acceleration and Speed on the x-axis and y-axis => Use velocity.
+    //float angle{};// => Use velocity in b2_body.
 
-    float spd[2]; //Acceleration and Speed on the x-axis and y-axis
-    float angle{};
-public:
-    virtual ~Character() = default;
-    virtual void draw(RenderWindow& window) = 0;
     //virtual void update(float deltaTime) = 0;
 };
 
@@ -33,13 +41,12 @@ class Mario : public Character {
 protected:
     Clock timer1, timer2;
     bool goRight, goUp, goLeft, goDown;
-    float acc[2], startJumpPosition;
-
 public:
-    Mario(float x, float y); //set accleration
-    //void update(float deltaTime) override;
-    void draw(RenderWindow& window) override;
+    Mario(float x, float y); //set Position, Velocity and JumpVelocity
+    void draw(RenderWindow& window, int state) override;
     void setPosition(float x, float y);
+    void Begin() override;
+    void Update(float deltaTime) override;
 };
 
 /*
