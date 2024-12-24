6970046416e728f46dfdd6302b62a8c3ebde2e08
NguyenHoang <nguyenislearning03@gmail.com>
2024-12-24 13:08:29 +0700

    Clear the main before merge commit

--- SuperMario/Physics.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <box2d/b2_world.h>
-class Physics {
-	//b2_world is a world that controls every bodies in this world.
-public:
-	void Init();
-	void Update(float deltaTime);
-	static b2World world; //Static object helps this class act as a mediator to interaction between Mario and Map. 
-};
